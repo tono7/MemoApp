ab3731d03a2110076c352d96ff0ec9fd735f91b2
t.ono <46275238+tono7@users.noreply.github.com>
2021-11-04 18:11:38 +0900

    Install ESLing

---
+++ src/compornets/Hello.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet} from 'react-native'
+
+function Hello() {
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    );
+}
+
+export default Hello;
