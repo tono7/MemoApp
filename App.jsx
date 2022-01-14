83d2becfa66643525648df985d9c72ced2fc9908
t.ono <46275238+tono7@users.noreply.github.com>
2022-01-14 13:50:39 +0900

    Replace KeyboardAvoidingView to KeyboardSafeView

--- App.jsx
+++ App.jsx
@@ -7,7 +7,7 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
 
--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -3,16 +3,17 @@ import {View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native';
 
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
+import KeyboardSafeView from "../components/KeyboardSafeView";
 
 export default function MemoCreateScreen() {
     return (
-        <KeyboardAvoidingView style={styles.container} behavior="height">
+        <KeyboardSafeView style={styles.container} >
             <AppBar />
             <View style={styles.inputcontainer}>
                 <TextInput value="" multiline style={styles.input} />
             </View>
             <CircleButton name="check" />
-        </KeyboardAvoidingView>
+        </KeyboardSafeView>
     );
 }
 
