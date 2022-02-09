3e4fce41cd36661227d4580c7ab669654250b996
t.ono <46275238+tono7@users.noreply.github.com>
2022-02-09 22:12:22 +0900

    Set Logout Button

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { StyleSheet, View } from "react-native";
 
-import MemoList from '../components/MemoList'
-import CircleButton from '../components/CircleButton'
+import MemoList from '../components/MemoList';
+import CircleButton from '../components/CircleButton';
+import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
-    return (
-        <View style={styles.container}>
-            <MemoList />
-            <CircleButton
-             name="plus"
-             onPress={() => {navigation.navigate ('MemoCreate'); }}
-            />
-      </View>
-    );
+  useEffect( () => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
+
+  return (
+     <View style={styles.container}>
+        <MemoList />
+        <CircleButton
+           name="plus"
+           onPress={() => {navigation.navigate ('MemoCreate'); }}
+        />
+     </View>
+  );
 }
 
 const styles = StyleSheet.create({
